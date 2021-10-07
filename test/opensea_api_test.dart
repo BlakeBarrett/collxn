b20e91033387c3e0a3fd3cfaaf1474f8d20b97bb
Blake Barrett <blake@blakebarrett.net>
2021-10-07 15:55:51 -0700

    Move files around.

--- test/opensea_api_test.dart
+++ test/opensea_api_test.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: avoid_print
 
-import 'package:collxn/OpenSea_api.dart';
+import 'package:collxn/opensea/opensea_api.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
