06952abf6b919fb2eb9cb345c7f4df863596d9c8
Blake Barrett <blake@blakebarrett.net>
2021-10-15 21:16:15 -0700

    iOS and macOS apps can now make network calls and have been opened in Xcode 13.

--- lib/opensea/opensea_api.dart
+++ lib/opensea/opensea_api.dart
@@ -2,6 +2,7 @@
 import 'dart:convert';
 import 'package:collxn/opensea/asset.dart';
 import 'package:collxn/opensea/collection.dart';
+import 'package:collxn/opensea/user_info.dart';
 import 'package:http/http.dart' as http;
 
 class OpenSea {
@@ -12,33 +13,54 @@ class OpenSea {
   static const nftTokenId =
       '81110918036404122233458993428491379538347624814123393756341562125755476869130';
 
+  Future<UserInfo> getUserInfoForWallet(final String wallet) async {
+    final String endpoint = 'https://api.opensea.io/api/v1/user/$wallet';
+    final response = await http.get(Uri.parse(endpoint));
+    if (response.statusCode == 200) {
+      return UserInfo.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load user info');
+    }
+  }
+
   /// Returns a map of the NFT token information for the given wallet address.
   Future<List<Asset>> getNFTsForWallet(final String wallet) async {
     final String endpoint =
         'https://api.opensea.io/api/v1/assets?owner=$wallet';
     final response = await http.get(Uri.parse(endpoint));
-    final responseMap = json.decode(response.body);
-    final assets = responseMap['assets'] as List<dynamic>;
-    return assets.map((element) {
-      return Asset.fromJson(element);
-    }).toList();
+    if (response.statusCode == 200) {
+      final responseMap = json.decode(response.body);
+      final assets = responseMap['assets'] as List<dynamic>;
+      return assets.map((element) {
+        return Asset.fromJson(element);
+      }).toList();
+    } else {
+      throw Exception('Failed to load NFTs');
+    }
   }
 
   Future<List<Collection>> getCollections(final String wallet) async {
     final String endpoint =
         'https://api.opensea.io/api/v1/collections?asset_owner=$wallet';
     final response = await http.get(Uri.parse(endpoint));
-    final collections = json.decode(response.body) as List<dynamic>;
-    return collections.map((item) {
-      return Collection.fromJson(item);
-    }).toList();
+    if (response.statusCode == 200) {
+      final collections = json.decode(response.body) as List<dynamic>;
+      return collections.map((item) {
+        return Collection.fromJson(item);
+      }).toList();
+    } else {
+      throw Exception('Failed to load collections');
+    }
   }
 
   Future<Asset> getNFT(final String wallet, final String tokenId) async {
     final String endpoint =
         'https://api.opensea.io/api/v1/asset/$wallet/$tokenId';
     final response = await http.get(Uri.parse(endpoint));
-    final assetJson = json.decode(response.body);
-    return Asset.fromJson(assetJson);
+    if (response.statusCode == 200) {
+      return Asset.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load NFT');
+    }
   }
 }
